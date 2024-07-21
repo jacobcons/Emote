2960b9fab029d1c6fd33102c94ebf7022bbaaf70
Jacob <jacobcons@gmail.com>
2024-07-21 20:32:40 +0000

    ping supabase

--- src/server.js
+++ src/server.js
@@ -4,3 +4,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
+
+// ping supabase on server startup and every 6 days to prevent shutdown
+await sql`SELECT 1`.execute(db);
+setInterval(
+  async () => await sql`SELECT 1`.execute(db),
+  1000 * 60 * 60 * 24 * 6,
+);
